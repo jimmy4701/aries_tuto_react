7d65f123296c8565da5ae62456348eaf8c42ab61
Jimmy <jeremy.simonklein@gmail.com>
2018-12-21 14:10:35 +0100

    Added some movies

--- src/database/movies.js
+++ src/database/movies.js
@@ -19,6 +19,78 @@ export const movies = [
     },
     {
         highlight: false,
+        id: "harry-potter",
+        title: "Harry Potter à l'école des sorciers",
+        description: "Un film sur la sorcellerie",
+        url: "https://www.youtube.com/watch?v=7m0-X95nG98",
+        embed: "https://www.youtube.com/embed/7m0-X95nG98",
+        category_id: "fantastic"
+    },
+    {
+        highlight: false,
+        id: "seigneur-des-anneaux",
+        title: "Seigneur des anneaux",
+        description: "Un film pour les controler tous",
+        url: "https://www.youtube.com/watch?v=cdyQoF2Q0Lg",
+        embed: "https://www.youtube.com/embed/cdyQoF2Q0Lg",
+        category_id: "fantastic"
+    },
+    {
+        highlight: false,
+        id: "harry-potter",
+        title: "Harry Potter à l'école des sorciers",
+        description: "Un film sur la sorcellerie",
+        url: "https://www.youtube.com/watch?v=7m0-X95nG98",
+        embed: "https://www.youtube.com/embed/7m0-X95nG98",
+        category_id: "fantastic"
+    },
+    {
+        highlight: false,
+        id: "seigneur-des-anneaux",
+        title: "Seigneur des anneaux",
+        description: "Un film pour les controler tous",
+        url: "https://www.youtube.com/watch?v=cdyQoF2Q0Lg",
+        embed: "https://www.youtube.com/embed/cdyQoF2Q0Lg",
+        category_id: "fantastic"
+    },
+    {
+        highlight: false,
+        id: "harry-potter",
+        title: "Harry Potter à l'école des sorciers",
+        description: "Un film sur la sorcellerie",
+        url: "https://www.youtube.com/watch?v=7m0-X95nG98",
+        embed: "https://www.youtube.com/embed/7m0-X95nG98",
+        category_id: "fantastic"
+    },
+    {
+        highlight: false,
+        id: "seigneur-des-anneaux",
+        title: "Seigneur des anneaux",
+        description: "Un film pour les controler tous",
+        url: "https://www.youtube.com/watch?v=cdyQoF2Q0Lg",
+        embed: "https://www.youtube.com/embed/cdyQoF2Q0Lg",
+        category_id: "fantastic"
+    },
+    {
+        highlight: false,
+        id: "harry-potter",
+        title: "Harry Potter à l'école des sorciers",
+        description: "Un film sur la sorcellerie",
+        url: "https://www.youtube.com/watch?v=7m0-X95nG98",
+        embed: "https://www.youtube.com/embed/7m0-X95nG98",
+        category_id: "fantastic"
+    },
+    {
+        highlight: false,
+        id: "seigneur-des-anneaux",
+        title: "Seigneur des anneaux",
+        description: "Un film pour les controler tous",
+        url: "https://www.youtube.com/watch?v=cdyQoF2Q0Lg",
+        embed: "https://www.youtube.com/embed/cdyQoF2Q0Lg",
+        category_id: "fantastic"
+    },
+    {
+        highlight: false,
         id: "inception",
         title: "Inception",
         description: "Un film de reve",
@@ -50,7 +122,7 @@ export const movies = [
         title: "Skyrim",
         description: "Un film de dragons",
         url: "https://www.youtube.com/watch?v=JSRtYpNRoN0",
-        embed: "https://www.youtube.com/embed/JSRtYpNRoN0",
+        embed: "https://www.youtube.com/embed/JSRtYpNRoN0  ",
         category_id: "sf"
     }
 ]
